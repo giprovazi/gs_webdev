8cacc0b2e837c797a0ec546bedb9b095ebbdb83e
Giovanni <giovannirprovazi@gmail.com>
2024-11-25 13:46:27 -0300

    Finalizando a pagina contato e ajustando script

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,18 @@
-alert('Bem-vindo ao Site GreenWind Solar!')
+alert("Bem-vindo ao Site GreenWind Solar!");
 
-function trocar(cor){
-    document.body.style.background = cor
+function trocar(cor) {
+    document.body.style.backgroundColor = cor;
 }
 
 function instagram() {
-    alert("Nosso instagram: @greenwindsolar_oficial")
+    alert("Nosso instagram: @greenwindsolar_oficial");
 }
 
 function twitter() {
-    alert("Nosso twitter: @greenwindsolar_oficial")
+    alert("Nosso twitter: @greenwindsolar_oficial");
 }
 
-let imagens =[
+let imagens = [
     "src/assets/img1_home.jpg",
     "src/assets/img2_home.jpg",
     "src/assets/img3_home.jpg"
@@ -21,111 +21,91 @@ let imagens =[
 let i = 0;
 let time = 3000;
 
-function slideShow(){
-    document.getElementById('image').src=imagens[i]
+function slideShow() {
+    document.getElementById("image").src = imagens[i];
     i++;
-    if(i == imagens.length) {i = 0;}
-    setTimeout('slideShow()', time)
+    if (i == imagens.length) {
+        i = 0;
+    }
+    setTimeout(slideShow, time);
 }
+
 slideShow();
 
-// Script Login
+const form = document.getElementById("form");
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
 
-// Declarando as variáveis
-
-const form =document.getElementById("form")
-const nome =document.getElementById("nome")
-const email =document.getElementById("email")
-const senha =document.getElementById("senha")
-
-// Evento que vai pegar o que acontece no formulário
-
-form.addEventListener('submit',(e)=>{
-    // Previne qualquer alteração no formulário
-    e.preventDefault()
-
-    // Chamar a função que vai checar os campos
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     if (checarCampos()) {
-        // Se todos os campos forem válidos, aguarda 3 segundos antes de redirecionar
         setTimeout(() => {
-            window.location = "quiz.html"
-        }, 3000) // 3000 milisegundos = 3 segundos
+            window.location = "quiz.html";
+        }, 3000);
     }
-})
+});
 
-// Função checarCampos
-function checarCampos(){
-    // Declarando as variáveis
-    // O método trim remove os espaços em branco no início e no fim do texto
-    const nomeValor = nome.value.trim()
-    const emailValor = email.value.trim()
-    const senhaValor = senha.value.trim()
+function checarCampos() {
+    const nomeValor = nome.value.trim();
+    const emailValor = email.value.trim();
+    const senhaValor = senha.value.trim();
+    let valida = true;
 
-    // Flag para verificar se todos os campos estão corretos
-    let valida = true
-
-    // Se o campo "nome" estiver vazio
-
-    if(nomeValor === ""){
-        validarErro(nome,"Preencha o campo nome")
-        valida = false // Marca como inválido
-    }
-    else{
-        validarSucesso(nome)
+    if (nomeValor === "") {
+        validarErro(nome, "Preencha o campo nome");
+        valida = false;
+    } else {
+        validarSucesso(nome);
     }
 
-    // Se o campo "email" estiver vazio
-
-    if(emailValor === ""){
-        validarErro(email, "Preencha o campo e-mail")
-        valida = false // Marca como inválido
-
-    } else if(!emailValor.endsWith(".com")) {
-        validarErro(email,"Preencha o campo e-mail")
-        valida = false // Marca como inválido
-    }
-    
-    else {
-        validarSucesso(email)
+    if (emailValor === "") {
+        validarErro(email, "Preencha o campo e-mail");
+        valida = false;
+    } else if (!emailValor.endsWith(".com")) {
+        validarErro(email, "O e-mail deve terminar com .com");
+        valida = false;
+    } else {
+        validarSucesso(email);
     }
 
-    // Se o campo "senha" estiver vazio
-
-    if(senhaValor === ""){
-        validarErro(senha, "Preencha o campo senha")
-        valida = false // Marca como inválido
-    }
-    else if(senhaValor.length < 8)
-    {
-        validarErro(senha,"A senha deve ter mais que 8 caracteres")
-        valida = false // Marca como inválido
-    }else {
-        validarSucesso(senha)
+    if (senhaValor === "") {
+        validarErro(senha, "Preencha o campo senha");
+        valida = false;
+    } else if (senhaValor.length < 8) {
+        validarErro(senha, "A senha deve ter mais que 8 caracteres");
+        valida = false;
+    } else {
+        validarSucesso(senha);
     }
 
-    // Retorna true se todos os campos forem válidos, caso contrário retorna false
-    return valida
+    return valida;
 }
 
-    // Validar error
+function validarErro(input, message) {
+    const controle = input.parentElement;
+    const small = controle.querySelector("small");
+    small.innerText = message;
+    controle.className = "controle error";
+}
 
-    function validarErro(input, message){
-        // Retorna para o elemento pai
-        const controle = input.parentElement;
-        // Pega o primeiro elemento small que o seletor encontrar
-        const small = controle.querySelector('small')
-        // Pega a mensagem no campo em caso de erro
-        small.innerText = message
-        // Pega as configurações de CSS da classe controle e error
-        controle.className = "controle error"
-    }
+function validarSucesso(input) {
+    const controle = input.parentElement;
+    controle.className = "controle sucesso";
+}
 
+function telefone() {
+    alert("Nosso telefone fixo é 11-98786-6300");
+}
 
-    // Validar sucesso
+function emailContato() {
+    alert("O nosso email é GreenWind@gmail.com");
+}
 
-    function validarSucesso(input){
-        // Retorna para o elemento pai
-        const controle = input.parentElement;
-        // Pega a classe do controle sucesso
-        controle.className = 'controle sucesso'
-    }
+function wpp() {
+    alert("Nosso Whatsapp é 11-91384-1530");
+}
+
+function insta() {
+    alert("Nosso instagram é @GreenWind_br");
+}
